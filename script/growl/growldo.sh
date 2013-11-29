ff1d680ec0f186772b9f407b8e0a686d2f59fe0c
Zhiwei Xiao <zwxiao@gmail.com>
2013-11-29 16:06:06 +0800

    [growl] implement growlnotify in shell script

--- script/growl/growldo.sh
+++ script/growl/growldo.sh
@@ -5,5 +5,7 @@ start=$(date +'%s')
 $cmd
 stop=$(date +'%s')
 let elapsed=$stop-$start
-ip=`echo $SSH2_CLIENT | awk '{print $1}'`
-python ~/app/growlnotify.py $ip "$cmd" "under [$PWD] completed in [$elapsed] secs"
+ssh_client=`echo $SSH_CLIENT $SSH2_CLIENT | awk '{print $1}'`
+host=${GROWL_HOST_IP:=$ssh_client}
+cd $(dirname $(readlink $0))
+python growlnotify.py "$host" "$cmd" "completed in [$elapsed] secs @ [$PWD]"
--- script/growl/growlnotify.py
+++ script/growl/growlnotify.py
@@ -2,20 +2,20 @@
 
 from netgrowl import * 
 import sys
-#import socket
+import socket
 
 #title = "Notification from " + socket.gethostname()
 #desc = "" if len(sys.argv) > 2
 if len(sys.argv) < 3:
 	exit()
 
-hostname="YinKe_10_1_148_160" #socket.gethostname() 
+hostname=socket.gethostname() 
 ip = sys.argv[1]
 title = sys.argv[2]
 desc = sys.argv[3]
 
 addr = (ip, GROWL_UDP_PORT) 
-s = socket(AF_INET,SOCK_DGRAM) 
+s = socket.socket(AF_INET,SOCK_DGRAM) 
 p = GrowlRegistrationPacket(application=hostname, password="000000") 
 p.addNotification(hostname + "Notifications", enabled=True)
 s.sendto(p.payload(), addr) 
